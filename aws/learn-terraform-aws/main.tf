e692275bdb682a000bb7af9692db1de3bde32893
alexsilvan84 <tecnico_alexsilva@hotmail.com>
2022-06-07 23:05:12 -0300

    Arquivos da aula de terraform

---
+++ aws/learn-terraform-aws/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  region = "us-east-1d"
+  access_key = ""
+  secret_key = ""
+  profile = "default"
+
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-09d56f8956ab235b3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform alex"
+  }
+}
